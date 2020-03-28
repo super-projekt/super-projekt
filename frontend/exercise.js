9ace7acebee0e0ee9e015c8ab4cc3ff3d6fde3a6
Filip Twardzik <twardzikf@gmail.com>
2020-03-28 16:19:52 +0100

    Fix minor bugs in exercise.js

--- frontend/exercise.js
+++ frontend/exercise.js
@@ -1,19 +1,22 @@
 let login = prompt('State the login', 'login');
+
 if (login === 'Admin') {
-    let pass = prompt('State the password', 'password');
+    
     for (let i = 3; i > 0; i--) {
+        let pass = prompt('State the password', 'password');
         if (pass == 'The Master') {
             alert('Welcome')
             break
-        } else if (pass == '' || null) {
+        } else if (pass === '' || null) {
             alert('Cancelled')
             break
         } else {
             alert(`Wrong password, you have ${i} tries left`)
         }
-    } else if (login == '' || null) {
-        alert('Cancelled');
-    } else {
-        'User unknown';
     }
+} else if (login == '' || null) {
+    alert('Cancelled');
+} else {
+    alert('User unknown');
+}
 
